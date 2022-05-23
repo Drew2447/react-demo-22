651f06508852b19d7f4e8bc04d276381d1b34fc5
Andrew Owens <drew2447@gmail.com>
2022-05-23 14:08:28 -0600

    ch2

--- src/App.js
+++ src/App.js
@@ -1,16 +1,37 @@
 import "./App.css";
+import Card from "./Card";
+import CardInline from "./CardInline";
 import JSXDemo from "./JSXDemo";
+let users = [
+  { id: 1, name: "Bob", about: "likes to Fish", initialVotes: 2 },
+  { id: 2, name: "Jill", about: "likes to Swim", initialVotes: 10 },
+  { id: 3, name: "George", about: "likes to Code and Paint", initialVotes: 5 },
+  { id: 4, name: "Sally", about: "likes to Paint" },
+];
 
-// JSX is not a string and it is not HTML
-// it an extension to javascript
-// it allows to write 'html' in js
-// this is a React Class Component(Later)
-
-// this is a React Functional Component(Prefered)
-// a function => return JSX
 function App() {
-return(
-  <JSXDemo />
+  const renderUsers = () => {
+    return users.map((user) => {
+      return (
+        <Card
+          key={user.id}
+          initialVotes={user.initialVotes}
+          name={user.name}
+          about={user.about}
+        />
+      );
+    });
+  };
+
+  return (
+    <>
+      <h1>App</h1>
+      {/* <Card name='Bob' about='likes to fish' />
+      <Card name='Jill' about='likes to swim'/> */}
+      {renderUsers()}
+
+      {/* <JSXDemo /> */}
+    </>
   );
 }
 
