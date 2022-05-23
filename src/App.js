52f6c3604fd1bacce746f4d2819eb49dfd077254
Andrew Owens <drew2447@gmail.com>
2022-05-23 12:10:27 -0600

    first change

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import JSXDemo from "./JSXDemo";
 
+// JSX is not a string and it is not HTML
+// it an extension to javascript
+// it allows to write 'html' in js
+// this is a React Class Component(Later)
+
+// this is a React Functional Component(Prefered)
+// a function => return JSX
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+return(
+  <JSXDemo />
   );
 }
 
